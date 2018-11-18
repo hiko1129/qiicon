7488826f934623d11981e09ede9ca5ac8b1b529a
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2018-11-19 01:54:33 +0900

    changed directory structure

--- client_test.go
+++ client_test.go
@@ -1,19 +1,20 @@
-package qiicon
+package qiicon_test
 
 import (
 	"testing"
 
+	"github.com/hiko1129/qiicon"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFetchTotalContributionCount(t *testing.T) {
-	c, err := New("hiko1129")
+	c, err := qiicon.New("hiko1129")
 	assert.NoError(t, err)
 
 	_, err = c.FetchTotalContributionCount()
 	assert.NoError(t, err)
 
-	c, _ = New("-1-1-1-")
+	c, _ = qiicon.New("-1-1-1-")
 	count, err := c.FetchTotalContributionCount()
 	assert.Error(t, err)
 	assert.Equal(t, 0, count)
